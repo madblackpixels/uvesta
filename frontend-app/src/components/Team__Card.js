05fa4dced1fde0baceee1e97757a3435f8d1414c
Игорь Челышкин <mr_igoss@MacBook-Pro-Igor.local>
2018-10-21 22:04:11 +0300

    test

--- frontend-app/src/components/Team__Card.js
+++ frontend-app/src/components/Team__Card.js
@@ -11,14 +11,13 @@ export default class Team__Card extends Component {
 
         return(
             <Col xs={12} sm={4} md={4} lg={4}>
-                <div>
-                    <Thumbnail src={this.props.team_item.image} className="thumbnail__sm">
-                        <h4 className="text-color__blood-red">
-                            {this.props.team_item.name}
-                        </h4>
-                        <p>{this.props.team_item.text}</p>
-                    </Thumbnail>
-                </div>
+
+                <Thumbnail src={this.props.team_item.image} className="thumbnail__sm">
+                    <h4 className="text-color__blood-red">
+                        {this.props.team_item.name}
+                    </h4>
+                    <p>{this.props.team_item.text}</p>
+                </Thumbnail>
             </Col>
         )
     }
